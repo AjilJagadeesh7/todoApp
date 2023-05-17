1c8f25d33ccaf311038e10adb2efd3dc80ad62f8
AjilJagadeesh7 <ajiljagadeesh7@gmail.com>
2023-05-17 11:22:02 +0530

    removed context provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
 import { Provider } from "react-redux";
 import TodoBody from "./components/todoBody";
-import { AlertConstateProvider } from "./contexts/alertConstate";
+
 import store from "./store/reduxStore";
 
 function App() {
   return (
-    <AlertConstateProvider>
-      <Provider store={store}>
-        <div className="flex justify-center h-screen items-center bg-slate-800">
-          <TodoBody />
-        </div>
-      </Provider>
-    </AlertConstateProvider>
+    <Provider store={store}>
+      <div className="flex justify-center h-screen items-center bg-slate-800">
+        <TodoBody />
+      </div>
+    </Provider>
   );
 }
 
